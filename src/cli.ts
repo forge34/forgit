59d748eed5f40772422507e7571b5cacec48f2a7
Forge <28455546+forge34@users.noreply.github.com>
2026-07-17 21:03:40 +0300

    refactor: move init to repository-class

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,5 @@
 import { program } from "@commander-js/extra-typings";
-import { existsSync, mkdirSync, writeFileSync } from "node:fs";
-import { lstat } from "node:fs/promises";
-import { join } from "node:path";
+import Repository from "./repository";
 
 program
   .name("forgit")
@@ -11,28 +9,7 @@ program
   .command("init [directory]")
   .description("Initiailize a new repo")
   .action(async (directory) => {
-    const path = join(process.cwd(), directory ?? ".");
-    const gitDir = join(path, ".git");
-
-    if (existsSync(path)) {
-      const stat = await lstat(path);
-      if (!stat.isDirectory()) {
-        console.log(`fatal: ${path} is not a directory`);
-        process.exit(1);
-      }
-    }
-
-    if (existsSync(gitDir)) {
-      console.log("fatal: .git already exists");
-      process.exit(1);
-    }
-
-    mkdirSync(gitDir, { recursive: true });
-    mkdirSync(join(path, ".git/objects"), { recursive: true });
-    mkdirSync(join(path, ".git/refs/heads"), { recursive: true });
-    mkdirSync(join(path, ".git/refs/tags"), { recursive: true });
-    writeFileSync(join(path, ".git/HEAD"), "ref: refs/heads/main\n");
-    console.log("Initialized git directory");
+    Repository.init(directory);
   });
 
 program.parse();
--- src/repository.ts
+++ src/repository.ts
@@ -1,5 +1,34 @@
+import { existsSync, mkdirSync, writeFileSync } from "node:fs";
+import { lstat } from "node:fs/promises";
+import { join } from "node:path";
+
 class Repository {
   constructor() {}
+
+  static async init(directoryPath: string | undefined) {
+    const path = join(process.cwd(), directoryPath ?? ".");
+    const gitDir = join(path, ".git");
+
+    if (existsSync(path)) {
+      const stat = await lstat(path);
+      if (!stat.isDirectory()) {
+        console.log(`fatal: ${path} is not a directory`);
+        process.exit(1);
+      }
+    }
+
+    if (existsSync(gitDir)) {
+      console.log("fatal: .git already exists");
+      process.exit(1);
+    }
+
+    mkdirSync(gitDir, { recursive: true });
+    mkdirSync(join(path, ".git/objects"), { recursive: true });
+    mkdirSync(join(path, ".git/refs/heads"), { recursive: true });
+    mkdirSync(join(path, ".git/refs/tags"), { recursive: true });
+    writeFileSync(join(path, ".git/HEAD"), "ref: refs/heads/main\n");
+    console.log("Initialized git directory");
+  }
 }
 
 export default Repository;
