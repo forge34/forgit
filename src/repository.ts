7f8058c91fa7cbd14e8652f5acf7f9816f85614b
Forge <28455546+forge34@users.noreply.github.com>
2026-07-17 21:37:54 +0300

    feat: implements repoFind & repoPath

--- src/repository.ts
+++ src/repository.ts
@@ -1,9 +1,36 @@
 import { existsSync, mkdirSync, writeFileSync } from "node:fs";
-import { lstat } from "node:fs/promises";
+import { lstat, realpath } from "node:fs/promises";
 import { join } from "node:path";
 
 class Repository {
-  constructor() {}
+  public gitDir: string;
+  public workTree: string;
+
+  constructor(path: string) {
+    this.workTree = path;
+    this.gitDir = join(path, ".git");
+  }
+
+  repoPath(...paths: string[]) {
+    return join(this.gitDir, ...paths);
+  }
+
+  static async repoFind(path = "."): Promise<Repository> {
+    const realPath = await realpath(path);
+    const gitDir = join(realPath, ".git");
+
+    if (existsSync(gitDir) && (await lstat(gitDir)).isDirectory()) {
+      return new Repository(realPath);
+    }
+
+    const parent = await realpath(join(realPath, ".."));
+    if (parent === realPath) {
+      console.log("fatal: not a git repository (or any parent up to root)");
+      process.exit(1);
+    }
+
+    return Repository.repoFind(parent);
+  }
 
   static async init(directoryPath: string | undefined) {
     const path = join(process.cwd(), directoryPath ?? ".");
@@ -28,6 +55,8 @@ class Repository {
     mkdirSync(join(path, ".git/refs/tags"), { recursive: true });
     writeFileSync(join(path, ".git/HEAD"), "ref: refs/heads/main\n");
     console.log("Initialized git directory");
+
+    return new Repository(path);
   }
 }
 
