0aa8258fa1f7cf05a7a5c0515db3f5b297931b3b
Forge <28455546+forge34@users.noreply.github.com>
2026-07-17 15:02:04 +0300

    refactor: prepare for code-restructure

--- src/repository.ts
+++ src/repository.ts
@@ -1,36 +1,5 @@
-import Branch from "./branch";
-import Commit from "./commit";
-
 class Repository {
-  public branches: Branch[] = [];
-  public main: Branch;
-  public head: Branch | null = null;
-
-  constructor() {
-    this.main = new Branch("main", null);
-    this.branches.push(this.main);
-    this.head = this.main;
-  }
-
-  commit(id: string, message: string) {
-    const parent = this.head?.commit ?? null;
-    const commit = new Commit(parent, id, message);
-    if (this.head) {
-      this.head.commit = commit;
-    }
-    return commit;
-  }
-
-  log() {
-    let history: Commit[] = [];
-    let commit = this.head?.commit ?? null;
-    while (commit) {
-      history.push(commit);
-      commit = commit.parent;
-    }
-
-    return history;
-  }
+  constructor() {}
 }
 
 export default Repository;
